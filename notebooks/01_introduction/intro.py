63dfed9982bf782cb9919459f40f8e755bdac298
Sneha <ssnehlata238@gmail.com>
2025-11-22 11:19:29 +0530

    .jupytext.toml

---
+++ notebooks/01_introduction/intro.py
@@ -0,0 +1,16 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.18.1
+#   kernelspec:
+#     display_name: Python (tf-env)
+#     language: python
+#     name: tf-env
+# ---
+
+# %%
